c67dfed65bb654a2529a87b129c3274a8ada5d65
Harsh Gautam <hgautam208@gmail.com>
2020-10-17 11:28:58 +0530

    more refactoring and added some comments

--- textutils/funcs.py
+++ textutils/funcs.py
@@ -1,4 +1,4 @@
-
+# Function to remove punctuations from text
 def removepunc(dftext):
     analyze = ''
     punctuations = '''"<!@#$%^&*()][}{;'.,/:?>'''
@@ -7,11 +7,13 @@ def removepunc(dftext):
             analyze += char
     return analyze
 
+# Function to convert text to Uppercase
 def toUpper(dftext):
     analyze = ''
     analyze = dftext.upper()
     return analyze
 
+# Function to remove extra spaces from the given text
 def removespaces(dftext):
     analyze = ''
     for index, char in enumerate(dftext):
@@ -21,10 +23,11 @@ def removespaces(dftext):
             analyze += char
     return analyze
 
+# Funtion to coun the number of characters in text. It couns total characters and each character's count.
 def charcount(dftext):
     analyze = ''
     dftext_len = len(dftext)
-    char_count = {key: 0 for key in tuple(dftext)}
+    char_count = {key: 0 for key in tuple(dftext)} # Dict comprehension for generating dictionary with keys of value 0
     for char in dftext:
         char_count[char] += 1
     analyze = analyze + "Total Characters: " + str(dftext_len) + "\nCharacters Count: " +  str(char_count)
--- textutils/views.py
+++ textutils/views.py
@@ -2,14 +2,16 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
-from textutils import funcs
+from textutils import funcs # funcs.py that has various functions for text analyzer
 
 def index(request):
     return render(request, 'index.html')
 
 def analyze(request):
+    # Get the text from textarea
     dftext = request.POST.get('text', 'default')
-    print(dftext)
+    
+    # Get values for various sqwitches/checkboxes
     removepunc_res = request.POST.get('removepunc', 'off')
     uppercase_res = request.POST.get('uppercase', 'off')
     removespace_res = request.POST.get('removespace', 'off')
@@ -21,23 +23,22 @@ def analyze(request):
             analyze = funcs.removepunc(dftext)
             dftext = analyze
             purpose += "Remove Punctuations, "
-            # params = {'purpose':'Remove Punctuations', 'analyzed_text': analyze}
+            
         if uppercase_res == 'on':
             analyze = funcs.toUpper(dftext)
             dftext = analyze
             purpose += "Uppercase, "
-            # params = {'purpose':'Uppercase', 'analyzed_text': analyze}
+            
         if removespace_res == 'on':
             analyze = funcs.removespaces(dftext)
             dftext = analyze
             purpose += "Remove Extra Spaces, "
-            # params = {'purpose':'Remove Extra Spaces', 'analyzed_text': analyze}
 
         if charcount_res == 'on':
             analyze = funcs.charcount(dftext)
             dftext += "\n\n" + analyze
             purpose += "Character Count, "
-            # params = {'purpose':'Character Count', 'analyzed_text': analyze}
+            
         params = {'purpose': purpose, 'analyzed_text': dftext}
     except:
         params = {'purpose': '', 'analyzed_text': "Select atleast one checkbox!!!"}
